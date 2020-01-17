a13aad959988f5963b6be3e49e88b3155e5e1958
rafael-aero <rafael@rafael.aero>
2020-01-17 17:06:18 +0100

    Add raw wrapper for HTML elements

---
+++ Tesserae/src/Components/Raw.cs
@@ -0,0 +1,23 @@
+﻿using Retyped;
+
+namespace Tesserae.Components
+{
+    public class Raw :IComponent, IHasMarginPadding, IHasBackgroundColor
+    {
+        private dom.HTMLElement InnerElement;
+
+        public Raw(dom.HTMLElement element)
+        {
+            InnerElement = element;
+        }
+
+        public string Background { get => InnerElement.style.background; set => InnerElement.style.background = value; }
+        public string Margin { get => InnerElement.style.margin; set => InnerElement.style.margin = value; }
+        public string Padding { get => InnerElement.style.padding; set => InnerElement.style.padding = value; }
+
+        public dom.HTMLElement Render()
+        {
+            return InnerElement;
+        }
+    }
+}
