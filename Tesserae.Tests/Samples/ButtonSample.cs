9413a3203c6d92b28a13f7fe2ca58ffa20c76f68
rafael-aero <rafael@rafael.aero>
2020-01-18 11:46:16 +0100

    Improve button sample

--- Tesserae.Tests/Samples/ButtonSample.cs
+++ Tesserae.Tests/Samples/ButtonSample.cs
@@ -15,15 +15,17 @@ namespace Tesserae.Tests.Samples
             var btn2 = Button();
             var iconBtn1 = Button();
             var iconBtn2 = Button();
-            content = Stack().Children(
+            content = SectionStack().Section(Stack().Children(
                 TextBlock("Button").XLarge(),
                 TextBlock("Overview").MediumPlus(),
                 TextBlock("Buttons are best used to enable a user to commit a change or complete steps in a task. They are typically found inside forms, dialogs, panels or pages. An example of their usage is confirming the deletion of a file in a confirmation dialog."),
                 TextBlock("When considering their place in a layout, contemplate the order in which a user will flow through the UI. As an example, in a form, the individual will need to read and interact with the form fields before submiting the form. Therefore, as a general rule, the button should be placed at the bottom of the UI container (a dialog, panel, or page) which holds the related UI elements."),
-                TextBlock("While buttons can technically be used to navigate a user to another part of the experience, this is not recommended unless that navigation is part of an action or their flow."),
+                TextBlock("While buttons can technically be used to navigate a user to another part of the experience, this is not recommended unless that navigation is part of an action or their flow."))
+                )
+                .Section(Stack().Children(
                 TextBlock("Best Practices").MediumPlus(),
                 Stack().Horizontal().Children(
-                    Stack().WidthPercents(40).Children(
+                    Stack().Children(
                         TextBlock("Do").Medium(),
                         TextBlock("Make sure the label conveys a clear purpose of the button to the user."),
                         TextBlock("Button labels must describe the action the button will perform and should include a verb. Use concise, specific, self-explanatory labels, usually a single word."),
@@ -37,15 +39,16 @@ namespace Tesserae.Tests.Samples
                         TextBlock("Default buttons should always perform safe operations. For example, a default button should never delete."),
                         TextBlock("Use task buttons to cause actions that complete a task or cause a transitional task. Do not use buttons to toggle other UX in the same context. For example, a button may be used to open an interface area but should not be used to open an additional set of components in the same interface.")
                     ),
-                    Stack().WidthPercents(40).Children(
+                    Stack().Children(
                         TextBlock("Don't").Medium(),
                         TextBlock("Don't use generic labels like \"Ok, \" especially in the case of an error; errors are never \"Ok.\""),
                         TextBlock("Don’t place the default focus on a button that destroys data. Instead, place the default focus on the button that performs the \"safe act\" and retains the content (i.e. \"Save\") or cancels the action (i.e. \"Cancel\")."),
                         TextBlock("Don’t use a button to navigate to another place, use a link instead. The exception is in a wizard where \"Back\" and \"Next\" buttons may be used."),
                         TextBlock("Don’t put too much text in a button - try to keep the length of your text to a minimum."),
                         TextBlock("Don't put anything other than text in a button.")
-                    )
-                ),
+                    ))
+                ))
+                .Section(Stack().Children(
                 TextBlock("Usage").MediumPlus(),
                 Toggle("Disable buttons").Checked().OnChanged((s, e) =>
                     {
@@ -60,9 +63,7 @@ namespace Tesserae.Tests.Samples
                 Stack().Horizontal().Children(
                     iconBtn1.Text("Standard").Icon("far fa-plus").OnClicked((s, e) => alert("Clicked!")),
                     iconBtn2.Text("Primary").Icon("far fa-minus").Primary().OnClicked((s, e) => alert("Clicked!"))
-                )
-
-            );
+                )));
         }
 
         public HTMLElement Render()
