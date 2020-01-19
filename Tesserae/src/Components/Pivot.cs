f7541316173eada38495b0278e49bd72f955ac78
rafael-aero <8791811+rafael-aero@users.noreply.github.com>
2020-01-19 17:58:01 +0100

    Update Pivot.cs

--- Tesserae/src/Components/Pivot.cs
+++ Tesserae/src/Components/Pivot.cs
@@ -44,7 +44,7 @@ namespace Tesserae.Components
         {
             title.onclick = (e) =>
             {
-                e.StopEvent();
+                StopEvent(e);
                 Select(id);
             };
 
