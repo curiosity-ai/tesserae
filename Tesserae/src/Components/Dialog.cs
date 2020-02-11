e939cfed66abf2e12cd84f588ab5ccca83c32a89
rafael-aero <rafael@rafael.aero>
2020-02-11 19:11:05 +0100

    missing blocking

--- Tesserae/src/Components/Dialog.cs
+++ Tesserae/src/Components/Dialog.cs
@@ -23,7 +23,7 @@ namespace Tesserae.Components
 
         public Dialog(IComponent content = null, IComponent title = null)
         {
-            _modal = Modal().CenterContent().HideCloseButton().NoLightDismiss();
+            _modal = Modal().CenterContent().HideCloseButton().NoLightDismiss().Blocking();
             _modal.SetHeader(title);
             _modal.Content = content;
             _modal._modal.classList.add("tss-dialog");
