931f5f5d170d5c264c43d32921704ec1b43a299a
rafael-aero <rafael@rafael.aero>
2020-02-12 11:06:44 +0100

    Update Breadcrumb.cs

--- Tesserae/src/Components/Breadcrumb.cs
+++ Tesserae/src/Components/Breadcrumb.cs
@@ -89,7 +89,7 @@ namespace Tesserae.Components
 
             if (_overflowIndex >= 0)
             {
-                for (int i = 0; i <= ((_overflowIndex)*2); i++)
+                for (int i = 0; i <= Math.Min(keep.Length - 1, ((_overflowIndex) * 2)); i++)
                 {
                     keep[i] = KEEP;
                     if ((i+1 < _overflowIndex-2))
