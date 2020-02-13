b8a70bf8a445ffda05438408ce3d968ff1ab21eb
mike.bowen <mike@curiosity.ai>
2020-02-13 18:16:15 +0000

    Updated Overview content on both BasicList and DetailsList components sample pages

--- Tesserae.Tests/Samples/BasicListSample.cs
+++ Tesserae.Tests/Samples/BasicListSample.cs
@@ -23,26 +23,25 @@ namespace Tesserae.Tests.Samples
                         Stack()
                             .Children(
                                 SampleTitle("Overview"),
-                                TextBlock("DetailsList is a derivative of the List component. It is a robust way to " +
-                                          "display an information rich collection of items. It can support powerful " +
-                                          "ways to aid a user in finding content with sorting, grouping and " +
-                                          "filtering.  Lists are a great way to handle large amounts of content, " +
-                                          "but poorly designed Lists can be difficult to parse. Use a DetailsList " +
-                                          "when density of information is critical. Lists can support single and " +
-                                          "multiple selection, as well as drag and drop and marquee selection. " +
-                                          "They are composed of a column header, which contains the metadata " +
-                                          "fields which are attached to the list items, and provide the ability " +
-                                          "to sort, filter and even group the list. List items are composed of " +
-                                          "selection, icon, and name columns at minimum. One can also include " +
-                                          "other columns such as Date Modified, or any other metadata field " +
-                                          "associated with the collection. Place the most important columns from " +
-                                          "left to right for ease of recall and comparison."),
-                                TextBlock("DetailsList is classically used to display files, but is also used to " +
-                                          "render custom lists that can be purely metadata. Avoid using file type " +
-                                          "icon overlays to denote status of a file as it can make the entire icon " +
-                                          "unclear. Be sure to leave ample width for each column’s data. " +
-                                          "If there are multiple lines of text in a column, " +
-                                          "consider the variable row height variant.")))
+                                TextBlock("List provides a base component for rendering large sets of items. " +
+                                          "It is agnostic of the tile component used, and selection " +
+                                          "management. These concerns can be layered separately.")
+                                    .PaddingBottom(Unit.Pixels, 16),
+                                TextBlock("Performance is important, and DOM content is expensive. Therefore, " +
+                                          "limit what you render. List applies this principle by using UI " +
+                                          "virtualization. Unlike a simple for loop that renders all items in " +
+                                          "a set, a List only renders a subset of items, and as you scroll around, " +
+                                          "the subset of rendered content is shifted. This gives a much " +
+                                          "better experience for large sets, especially when the " +
+                                          "per-item components are complex/render-intensive/network-intensive.")
+                                    .PaddingBottom(Unit.Pixels, 16),
+                                TextBlock("List breaks down the set of items passed in into pages. Only pages " +
+                                          "within a 'materialized window' are actually rendered. As that window " +
+                                          "changes due to scroll events, pages that fall outside that window are " +
+                                          "removed, and their layout space is remembered and pushed into spacer " +
+                                          "elements. This gives the user the experience of browsing massive amounts " +
+                                          "of content but only using a small number of actual elements. " +
+                                          "This gives the browser much less layout to resolve.")))
                     .Section(
                         Stack()
                             .Children(
