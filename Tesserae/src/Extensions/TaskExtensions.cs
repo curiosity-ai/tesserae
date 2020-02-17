a1ae5ce336a35ba969922f3e228c4fd72fc2f521
rafael-aero <rafael@rafael.aero>
2020-02-17 19:28:34 +0100

    improve TaskExtensions exception handling

--- Tesserae/src/Extensions/TaskExtensions.cs
+++ Tesserae/src/Extensions/TaskExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using static Retyped.dom;
+using System.Linq;
 
 namespace Tesserae.Components
 {
@@ -24,9 +25,28 @@ namespace Tesserae.Components
                 {
                     await task;
                 }
+                catch(AggregateException age)
+                {
+                    if (age.InnerExceptions.Count == 1)
+                    {
+                        console.log("Error running FireAndForget Task:" + age.InnerExceptions.Single().Message);
+                        console.log(age.InnerExceptions.Single());
+                    }
+                    else
+                    {
+
+                        console.log("Error running FireAndForget Task:" + age.Message);
+                        foreach (var inner in age.InnerExceptions)
+                        {
+                            console.log("\t\tInner exception:" + inner.Message);
+                            console.log(inner);
+                        }
+                    }
+                }
                 catch (Exception E)
                 {
-                    console.log("Error running FireAndForget Task: " + E.ToString());
+                    console.log("Error running FireAndForget Task:" + E.Message);
+                    console.log(E);
                 }
             });
         }
@@ -35,21 +55,7 @@ namespace Tesserae.Components
         /// Sometimes you want to start a Task and not await its results but there is an analyzer that presumes that code that creates Tasks and doesn't await them is incorrect
         /// (and, often, it is right) but sometimes you don't want to await and you don't want the analyzer telling you about it - in that case, use this extensions method
         /// </summary>
-        public static void FireAndForget<T>(this Task<T> task)
-        {
-            if (task is null) return;
-            Task.Run(async () =>
-            {
-                try
-                {
-                    await task;
-                }
-                catch (Exception E)
-                {
-                    console.log("Error running FireAndForget Task: " + E.ToString());
-                }
-            });
-        }
+        public static void FireAndForget<T>(this Task<T> task) => FireAndForget((Task)task);
 
         public static async Task<T> Unwrap<T>(this Task<Task<T>> task)
         {
@@ -106,6 +112,26 @@ namespace Tesserae.Components
         {
             await Task.WhenAll(t1, t2, t3, t4, t5, t6);
             return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result);
+        }
+
+        /// <summary>
+        /// Given multiple tasks that return values, wait for them all to complete and then return a tuple that contains the results (in the order in which the tasks are specified as method arguments).
+        /// If any of the tasks fail then an exception will be raised.
+        /// </summary>
+        public async static Task<(T1, T2, T3, T4, T5, T6, T7)> WhenAll<T1, T2, T3, T4, T5, T6, T7>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result);
+        }
+
+        /// <summary>
+        /// Given multiple tasks that return values, wait for them all to complete and then return a tuple that contains the results (in the order in which the tasks are specified as method arguments).
+        /// If any of the tasks fail then an exception will be raised.
+        /// </summary>
+        public async static Task<(T1, T2, T3, T4, T5, T6, T7, T8)> WhenAll<T1, T2, T3, T4, T5, T6, T7, T8>(Task<T1> t1, Task<T2> t2, Task<T3> t3, Task<T4> t4, Task<T5> t5, Task<T6> t6, Task<T7> t7, Task<T8> t8)
+        {
+            await Task.WhenAll(t1, t2, t3, t4, t5, t6, t7, t8);
+            return (t1.Result, t2.Result, t3.Result, t4.Result, t5.Result, t6.Result, t7.Result, t8.Result);
         }
 
         private static Task BuildCompletedTask()
