50fda44816ff6fb21e819cfb4a9cecdf56bbcdc2
rafael-aero <rafael@rafael.aero>
2020-02-20 11:03:12 +0100

    Update ModalSample.cs

--- Tesserae.Tests/Samples/ModalSample.cs
+++ Tesserae.Tests/Samples/ModalSample.cs
@@ -15,6 +15,8 @@ namespace Tesserae.Tests.Samples
             Modal("Lorem Ipsum")
                 .Var(out var modal)
                 .LightDismiss()
+                .Width(60.vw())
+                .Height(60.vh())
                 .SetFooter(TextBlock("This is a footer note").SemiBold().MediumPlus())
                 .Content(Stack().Children(
                             TextBlock("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lorem nulla, malesuada ut sagittis sit amet, vulputate in leo. Maecenas vulputate congue sapien eu tincidunt. Etiam eu sem turpis. Fusce tempor sagittis nunc, ut interdum ipsum vestibulum non. Proin dolor elit, aliquam eget tincidunt non, vestibulum ut turpis. In hac habitasse platea dictumst. In a odio eget enim porttitor maximus. Aliquam nulla nibh, ullamcorper aliquam placerat eu, viverra et dui. Phasellus ex lectus, maximus in mollis ac, luctus vel eros. Vivamus ultrices, turpis sed malesuada gravida, eros ipsum venenatis elit, et volutpat eros dui et ante. Quisque ultricies mi nec leo ultricies mollis. Vivamus egestas volutpat lacinia. Quisque pharetra eleifend efficitur. "),
