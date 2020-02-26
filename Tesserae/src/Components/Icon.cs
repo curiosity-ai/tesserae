f2a7e32f9d6e6528faa846545f93c7a8e55a441f
rafael-aero <rafael@rafael.aero>
2020-02-26 16:07:36 +0100

    Add color to icon

--- Tesserae/src/Components/Icon.cs
+++ Tesserae/src/Components/Icon.cs
@@ -3,11 +3,13 @@ using static Retyped.dom;
 
 namespace Tesserae.Components
 {
-    public class Icon : IComponent
+    public class Icon : IComponent, IHasForegroundColor
     {
         private readonly HTMLElement _icon;
 
         public Icon(string icon)    => _icon = I(_(icon));
+
+        public string Foreground { get => _icon.style.color; set => _icon.style.color = value; }
 
         public HTMLElement Render() => _icon;
     }
