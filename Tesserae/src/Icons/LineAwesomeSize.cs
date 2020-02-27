5b4f2724c79f9c218764ab4e0fd92496f5f867d5
rafael-aero <rafael@rafael.aero>
2020-02-27 20:44:07 +0100

    remove LineAwesome sizing, use Tesserae TextSize enum instead

--- Tesserae/src/Icons/LineAwesomeSize.cs
+++ Tesserae/src/Icons/LineAwesomeSize.cs
@@ -20,19 +20,4 @@ namespace Tesserae
         [Name("lab")]
         Brand
     }
-
-    [Enum(Emit.Value)]
-    public enum LineAwesomeSize
-    {
-        [Name("la-1x")] x1,
-        [Name("la-2x")] x2,
-        [Name("la-3x")] x3,
-        [Name("la-4x")] x4,
-        [Name("la-5x")] x5,
-        [Name("la-6x")] x6,
-        [Name("la-7x")] x7,
-        [Name("la-8x")] x8,
-        [Name("la-9x")] x9,
-        [Name("la-10x")] x10
-    }
 }
