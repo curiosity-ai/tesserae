6d2f26e712456cdb2cac17ac5df6224c15be8473
rafael-aero <rafael@rafael.aero>
2020-04-27 14:32:49 +0200

    missing color for sidebar

--- Tesserae/src/Base/UI.Theme.cs
+++ Tesserae/src/Base/UI.Theme.cs
@@ -57,6 +57,7 @@ namespace Tesserae
                 var sb = new StringBuilder();
                 sb.AppendLine(":root {");
                 sb.Append("  --tss-primary-background-color: ").Append(primaryLightColor.ToRGB()).AppendLine(";");
+                sb.Append("  --tss-sidebar-background-color: ").Append(primaryLightColor.ToRGB()).AppendLine(";");
                 sb.Append("  --tss-link-color: ").Append(primaryLightColor.ToRGB()).AppendLine(";");
                 sb.Append("  --tss-primary-border-color: ").Append(borderColorLight.ToRGB()).AppendLine(";");
                 sb.Append("  --tss-primary-background-hover-color: ").Append(borderColorLight.ToRGB()).AppendLine(";");
@@ -66,6 +67,7 @@ namespace Tesserae
 
                 sb.AppendLine(".tss-dark-mode {");
                 sb.Append("  --tss-primary-background-color: ").Append(primaryDarkColor.ToRGB()).AppendLine(";");
+                sb.Append("  --tss-sidebar-background-color: ").Append(primaryDarkColor.ToRGB()).AppendLine(";");
                 sb.Append("  --tss-link-color: ").Append(primaryDarkColor.ToRGB()).AppendLine(";"); 
                 sb.Append("  --tss-primary-border-color: ").Append(borderColorDark.ToRGB()).AppendLine(";");
                 sb.Append("  --tss-primary-background-hover-color: ").Append(borderColorDark.ToRGB()).AppendLine(";");
