425fd17ae7ea0962680085b2ab3535080dbfc26f
rafael-aero <rafael@rafael.aero>
2020-04-28 13:34:06 +0200

    Change CombinedObservable to use the interface

--- Tesserae/src/Helpers/Observable`1.cs
+++ Tesserae/src/Helpers/Observable`1.cs
@@ -49,15 +49,15 @@ namespace Tesserae
 
     public class CombinedObservable<T1, T2> : Observable, IObservable<(T1 first, T2 second)>
     {
-        private readonly Observable<T1> _first;
-        private readonly Observable<T2> _second;
+        private readonly IObservable<T1> _first;
+        private readonly IObservable<T2> _second;
         private double _refreshTimeout;
 
         public (T1 first, T2 second) Value => (_first.Value, _second.Value);
 
         public event ObservableEvent.ValueChanged<(T1 first, T2 second)> onValueChanged;
 
-        public CombinedObservable(Observable<T1> o1, Observable<T2> o2)
+        public CombinedObservable(IObservable<T1> o1, IObservable<T2> o2)
         {
             o1.onValueChanged += FirstValueChanged;
             o2.onValueChanged += SecondValueChanged;
