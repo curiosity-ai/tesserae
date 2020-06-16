curiosity-ai/tesserae

commit 1f782966bc2c4fd461555d5666bd22f1df78f0b5

Simplify logic around observables

Tesserae/src/Helpers/ObservableDictionary.cs
@@ -4,7 +4,7 @@ using static H5.Core.dom;
 
 namespace Tesserae
 {
-    public class ObservableDictionary<TKey, TValue> : Observable, IDictionary<TKey, TValue>, IObservable<IReadOnlyDictionary<TKey, TValue>>
+    public class ObservableDictionary<TKey, TValue> : IDictionary<TKey, TValue>, IObservable<IReadOnlyDictionary<TKey, TValue>>
     {
         public event ObservableEvent.ValueChanged<IReadOnlyDictionary<TKey, TValue>> onValueChanged;
         public IReadOnlyDictionary<TKey, TValue> Value => _dictionary;
@@ -34,17 +34,17 @@ namespace Tesserae
 
         private void HookValue(TValue v)
         {
-            if (_valueIsObservable && v is object)
+            if (_valueIsObservable && v is IObservable<TValue> observable)
             {
-                ((IObservable)v).OnChange(RaiseOnValueChanged);
+                observable.onValueChanged += RaiseOnValueChanged;
             }
         }
 
         private void UnhookValue(TValue v)
         {
-            if (_valueIsObservable && v is object)
+            if (_valueIsObservable && v is IObservable<TValue> observable)
             {
-                ((IObservable)v).Unobserve(RaiseOnValueChanged);
+                observable.onValueChanged -= RaiseOnValueChanged;
             }
         }
 
@@ -56,14 +56,13 @@ namespace Tesserae
             valueGetter(_dictionary);
         }
 
-        private void RaiseOnValueChanged()
+        private void RaiseOnValueChanged(TValue value)
         {
             window.clearTimeout(_refreshTimeout);
             _refreshTimeout = window.setTimeout(raise, 1);
             void raise(object t)
             {
                 onValueChanged?.Invoke(_dictionary);
-                RaiseOnChanged();
             }
         }
 
@@ -78,7 +77,7 @@ namespace Tesserae
                 }   
                 _dictionary[key] = value; 
                 HookValue(value);
-                RaiseOnValueChanged();
+                RaiseOnValueChanged(value);
             }
         }
 
@@ -94,14 +93,14 @@ namespace Tesserae
         {
             _dictionary.Add(key, value);
             HookValue(value);
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(value);
         }
 
         public void Add(KeyValuePair<TKey, TValue> item)
         {
             _dictionary.Add(item.Key, item.Value);
             HookValue(item.Value);
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(item.Value);
         }
 
         public void Clear()
@@ -115,7 +114,7 @@ namespace Tesserae
             }
 
             _dictionary.Clear();
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(default);
         }
 
         public bool Contains(KeyValuePair<TKey, TValue> item)
@@ -144,7 +143,7 @@ namespace Tesserae
             {
                 _dictionary.Remove(key);
                 UnhookValue(prev);
-                RaiseOnValueChanged();
+                RaiseOnValueChanged(default);
                 return true;
             }
             return false;
@@ -156,7 +155,7 @@ namespace Tesserae
             {
                 _dictionary.Remove(item.Key);
                 UnhookValue(prev);
-                RaiseOnValueChanged();
+                RaiseOnValueChanged(default);
                 return true;
             }
             return false;

Tesserae/src/Helpers/ObservableList.cs
@@ -5,7 +5,7 @@ using static H5.Core.dom;
 
 namespace Tesserae
 {
-    public class ObservableList<T> : Observable, IList<T>, ICollection<T>, IObservable<IReadOnlyList<T>>
+    public class ObservableList<T> : IList<T>, ICollection<T>, IObservable<IReadOnlyList<T>>
     {
         public event ObservableEvent.ValueChanged<IReadOnlyList<T>> onValueChanged;
 
@@ -34,17 +34,17 @@ namespace Tesserae
 
         private void HookValue(T v)
         {
-            if (_valueIsObservable && (v is IObservable observableV))
+            if (_valueIsObservable && (v is IObservable<T> observableV))
             {
-                observableV.OnChange(RaiseOnValueChanged);
+                observableV.onValueChanged += RaiseOnValueChanged;
             }
         }
 
         private void UnhookValue(T v)
         {
-            if (_valueIsObservable && (v is IObservable observableV))
+            if (_valueIsObservable && (v is IObservable<T> observableV))
             {
-                observableV.Unobserve(RaiseOnValueChanged);
+                observableV.onValueChanged -= RaiseOnValueChanged;
             }
         }
 
@@ -64,18 +64,17 @@ namespace Tesserae
                     UnhookValue(_list[index]);
                 }
                 _list[index] = value;
-                RaiseOnValueChanged();
+                RaiseOnValueChanged(value);
             }
         }
 
-        private void RaiseOnValueChanged()
+        private void RaiseOnValueChanged(T value)
         {
             window.clearTimeout(_refreshTimeout);
             _refreshTimeout = window.setTimeout(raise, 1);
             void raise(object t)
             {
                 onValueChanged?.Invoke(_list);
-                RaiseOnChanged();
             }
         }
 
@@ -89,7 +88,7 @@ namespace Tesserae
         {
             _list.Add(item);
             HookValue(item);
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(item);
         }
 
         public void AddRange(IEnumerable<T> enumerable)
@@ -98,8 +97,8 @@ namespace Tesserae
             {
                 _list.Add(item);
                 HookValue(item);
+                RaiseOnValueChanged(item);
             }
-            RaiseOnValueChanged();
         }
 
         public void Clear()
@@ -111,9 +110,8 @@ namespace Tesserae
                     UnhookValue(i);
                 }
             }
-
             _list.Clear();
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(default);
         }
 
         public bool Contains(T item) => _list.Contains(item);
@@ -135,7 +133,7 @@ namespace Tesserae
 
             _list.Insert(index, item);
             HookValue(item);
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(item);
         }
 
         public bool Remove(T item)
@@ -144,7 +142,7 @@ namespace Tesserae
             if (removed)
             {
                 UnhookValue(item);
-                RaiseOnValueChanged();
+                RaiseOnValueChanged(item);
             }
             return removed;
         }
@@ -157,7 +155,7 @@ namespace Tesserae
             }
 
             _list.RemoveAt(index);
-            RaiseOnValueChanged();
+            RaiseOnValueChanged(default);
         }
     }
 }

Tesserae/src/Helpers/IObservable.cs
@@ -2,7 +2,5 @@
 {
     public interface IObservable
     {
-        void OnChange(Observable.Changed changed);
-        void Unobserve(Observable.Changed changed);
     }
 }

Tesserae/src/Helpers/IObservable`1.cs
@@ -9,6 +9,5 @@
     {
         event ObservableEvent.ValueChanged<T> onValueChanged;
         T Value { get; }
-        void Observe(ObservableEvent.ValueChanged<T> onChange);
     }
 }

Tesserae/src/Helpers/Observable.cs
@@ -1,15 +1,0 @@
-﻿namespace Tesserae
-{
-    public class Observable
-    {
-        public delegate void Changed();
-        
-        private event Changed OnChanged;
-
-        public void OnChange(Changed changed) => OnChanged += changed; // TODO [2020-03-05 DWR]: Why does this method exist if we already have a public event that can be listened to?
-
-        public void Unobserve(Changed changed) => OnChanged -= changed; // TODO [2020-03-05 DWR]: Why does this method exist if we already have a public event that listeners can be removed from?
-
-        protected void RaiseOnChanged() => OnChanged?.Invoke();
-    }
-}

Tesserae/src/Helpers/Observable`1.cs
@@ -6,7 +6,7 @@ namespace Tesserae
     /// Enables monitoring of changes for a variable of type T (this class is for listeners only, if updating the value is required then the SettableObserver should be used)
     /// </summary>
     /// <typeparam name="T">An immutable type to be observed. Be careful with non-imutable types, as they may be changed in ways that will not be repoted here</typeparam>
-    public class Observable<T> : Observable, IObservable<T>
+    public class Observable<T> : IObservable<T>
     {
         private T _value;
         private double _refreshTimeout;
@@ -35,7 +35,6 @@ namespace Tesserae
             void raise(object t)
             {
                 onValueChanged?.Invoke(_value);
-                RaiseOnChanged();
             }
         }
 
@@ -47,7 +46,7 @@ namespace Tesserae
         }
     }
 
-    public class CombinedObservable<T1, T2> : Observable, IObservable<(T1 first, T2 second)>
+    public class CombinedObservable<T1, T2> : IObservable<(T1 first, T2 second)>
     {
         private readonly IObservable<T1> _first;
         private readonly IObservable<T2> _second;
@@ -82,7 +81,6 @@ namespace Tesserae
             void raise(object t)
             {
                 onValueChanged?.Invoke(Value);
-                RaiseOnChanged();
             }
         }
 

commit f4a6a1af91ba1c13b04d75ed2e08997f9e17ccaa

Fix the ObservableList and ObservableDictionary HookValue/UnhookValue methods - for cases where those collections were also of IOBservable implementations, the intention was that any change on any of those listed values would result in a change being reported by the ObservableList/Dictionary (this broke in commit 1f782966bc2c4fd461555d5666bd22f1df78f0b5#diff-9730f8d3397dc4ba05e102906fa9e292)
In the changeset linked below, it can be seen that the logic could only work if each item of type T was also an IObservable<T>. This is a very uncommon case, it's more likely that there could be an ObservableList of SettableObservable<int> instances and while SettableObservable<int> implements IObservable of SOMETHING (int), it doesn't implement IObservable of the same generic type T as the ObservableList (whose T is SettableObservable<int>) -

https://github.com/curiosity-ai/tesserae/commit/1f782966bc2c4fd461555d5666bd22f1df78f0b5

Tesserae/src/Helpers/ObservableDictionary.cs
@@ -16,13 +16,13 @@ namespace Tesserae
         public ObservableDictionary()
         {
             _dictionary = new Dictionary<TKey, TValue>();
-            _valueIsObservable = typeof(TValue).IsObservable();
+            _valueIsObservable = PossibleObservableHelpers.IsObservable(typeof(TValue));
         }
 
         public ObservableDictionary(Dictionary<TKey, TValue> dictionary)
         {
             _dictionary = dictionary;
-            _valueIsObservable = typeof(TValue).IsObservable();
+            _valueIsObservable = PossibleObservableHelpers.IsObservable(typeof(TValue));
             if (_valueIsObservable)
             {
                 foreach (var kv in _dictionary)
@@ -45,14 +45,14 @@ namespace Tesserae
 
         private void HookValue(TValue v)
         {
-            if (_valueIsObservable && (v is IObservable<TValue> observable))
-                observable.ObserveFutureChanges(RaiseOnValueChanged);
+            if (_valueIsObservable)
+                PossibleObservableHelpers.ObserveFutureChangesIfObservable(v, RaiseOnValueChanged);
         }
 
         private void UnhookValue(TValue v)
         {
             if (_valueIsObservable && v is IObservable<TValue> observable)
-                observable.StopObserving(RaiseOnValueChanged);
+                PossibleObservableHelpers.StopObservingIfObservable(v, RaiseOnValueChanged);
         }
 
         private void RaiseOnValueChanged()

Tesserae/src/Helpers/ObservableList.cs
@@ -16,13 +16,13 @@ namespace Tesserae
         public ObservableList()
         {
             _list = new List<T>();
-            _valueIsObservable = typeof(T).IsObservable();
+            _valueIsObservable = PossibleObservableHelpers.IsObservable(typeof(T));
         }
 
         public ObservableList(params T[] initialValues)
         {
             _list = initialValues.ToList();
-            _valueIsObservable = typeof(T).IsObservable();
+            _valueIsObservable = PossibleObservableHelpers.IsObservable(typeof(T));
             if (_valueIsObservable)
             {
                 foreach (var i in _list)
@@ -34,14 +34,14 @@ namespace Tesserae
 
         private void HookValue(T v)
         {
-            if (_valueIsObservable && (v is IObservable<T> observableV))
-                observableV.ObserveFutureChanges(RaiseOnValueChanged);
+            if (_valueIsObservable)
+                PossibleObservableHelpers.ObserveFutureChangesIfObservable(v, RaiseOnValueChanged);
         }
 
         private void UnhookValue(T v)
         {
-            if (_valueIsObservable && (v is IObservable<T> observableV))
-                observableV.StopObserving(RaiseOnValueChanged);
+            if (_valueIsObservable)
+                PossibleObservableHelpers.StopObservingIfObservable(v, RaiseOnValueChanged);
         }
 
         public void Observe(ObservableEvent.ValueChanged<IReadOnlyList<T>> valueGetter) => Observe(valueGetter, callbackImmediately: true);

Tesserae/src/Helpers/PossibleObservableHelpers.cs
@@ -1,0 +1,86 @@
+using System;
+using System.Reflection;
+
+namespace Tesserae
+{
+    internal static class PossibleObservableHelpers
+    {
+        /// <summary>
+        /// Is this type one that either is directly an IObservable&lt;T&gt; or one that is derived from one?
+        /// </summary>
+        public static bool IsObservable(Type source) => TryToGetFirstWrappedValueFromAnIsObservable(source) == null;
+
+        /// <summary>
+        /// If the specified 'source' object implements any IObservable&lt;T&gt; interface then the 'receiver' Action will be registered with it via ObserveFutureChanges - this will be a no-op for a null
+        /// 'source' but the 'receiver' delegate must not be null
+        /// </summary>
+        public static bool ObserveFutureChangesIfObservable(object source, Action receiver) => UpdateObservingStatusIfObservable(source, receiver, listenForFutureChanges: true);
+
+        /// <summary>
+        /// If the specified 'source' object implements any IObservable&lt;T&gt; interface then the 'receiver' Action will be unregistered with it via StopObserving - this will be a no-op for a null
+        /// 'source' but the 'receiver' delegate must not be null
+        /// </summary>
+        public static bool StopObservingIfObservable(object source, Action receiver) => UpdateObservingStatusIfObservable(source, receiver, listenForFutureChanges: false);
+
+        private static Type TryToGetFirstWrappedValueFromAnIsObservable(Type type)
+        {
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (IsAnIObservableInterface(type, out var wrappedValueType))
+                return wrappedValueType;
+
+            foreach (var i in type.GetInterfaces())
+            {
+                if (IsAnIObservableInterface(i, out wrappedValueType))
+                    return wrappedValueType;
+            }
+            return null;
+        }
+
+        private static bool IsAnIObservableInterface(Type type, out Type wrappedValueType)
+        {
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
+
+            if (!type.IsGenericType || (type.GetGenericTypeDefinition() == typeof(IObservable<>)))
+            {
+                wrappedValueType = null;
+                return false;
+            }
+            wrappedValueType = type.GetGenericArguments()[0];
+            return true;
+        }
+
+        private static bool UpdateObservingStatusIfObservable(object source, Action receiver, bool listenForFutureChanges)
+        {
+            if (receiver is null)
+                throw new ArgumentNullException(nameof(receiver));
+
+            if (source is null)
+                return false;
+
+            var wrappedValueTypeIfSourceIsAnObserverable = TryToGetFirstWrappedValueFromAnIsObservable(source.GetType());
+            if (wrappedValueTypeIfSourceIsAnObserverable is null)
+                return false;
+
+            // Two very important notes:
+            //  1. We know that source is an instance of IObservable<T> and we know what T is but we need reflection to hook it up such that we can call ObserveFutureChanges or StopObserving on the
+            //     generic types when we only know the generic type information at runtime, not compile time.
+            //  2. When we pass the "receiver" (that is type Action) to ObserveFutureChanges or StopObserving, we might worry that it won't be accepted by those methods because they expect methods
+            //     that are of type ObservableEvent.ValueChanged<T> and so which receive an argument (which, of course, Action does not). However, it doesn't matter to JavaScript if a function is
+            //     called with too many parameters AND there is a really important gotcha to be aware of; if we wrapped the Action receiver up in a lambda to change it into a ValueChanged<T> then
+            //     we would be passing an anonymous function reference to ObserveFutureChanges.. which would work but things would go wrong when we tried to call StopObserving because we would get
+            //     a NEW anonymous function reference, which wouldn't appear as one of the items on the underlying observable's OnValueChanged invocation list and so the StopObserving would silently
+            //     fail, in effect.
+            var methodName = listenForFutureChanges ? nameof(HookCallbackForFutureChanges) : nameof(UnhookCallbackForFutureChanges);
+            var unboundMethod = typeof(PossibleObservableHelpers).GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static);
+            var boundMethod = unboundMethod.MakeGenericMethod(wrappedValueTypeIfSourceIsAnObserverable).Invoke(null, source, receiver); // "obj" is null for a static method
+            return true;
+        }
+
+        private static void HookCallbackForFutureChanges<T>(IObservable<T> observable, ObservableEvent.ValueChanged<T> receiver) => observable.ObserveFutureChanges(receiver);
+        
+        private static void UnhookCallbackForFutureChanges<T>(IObservable<T> observable, ObservableEvent.ValueChanged<T> receiver) => observable.StopObserving(receiver);
+    }
+}

Tesserae/src/Helpers/TypeExtensions.cs
@@ -1,12 +1,0 @@
-using System;
-
-namespace Tesserae
-{
-    internal static class TypeExtensions
-    {
-        public static bool IsObservable(this Type source)
-        {
-            return typeof(IBaseObservable).IsAssignableFrom(source);
-        }
-    }
-}