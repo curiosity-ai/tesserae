a937b053942c9ea8b6f1ea1829386402b4b71b5d
rafael-aero <rafael@rafael.aero>
2020-07-14 15:38:15 +0200

    Add fit mode for Image component

--- Tesserae/src/Components/Image.cs
+++ Tesserae/src/Components/Image.cs
@@ -15,5 +15,38 @@ namespace Tesserae.Components
         {
             return InnerElement;
         }
+
+        public Image Fit(ImageFit fit)
+        {
+            switch (fit)
+            {
+                case ImageFit.Contain:
+                    InnerElement.style.objectFit = "contain";
+                    break;
+                case ImageFit.Cover:
+                    InnerElement.style.objectFit = "cover";
+                    break;
+                case ImageFit.Fill:
+                    InnerElement.style.objectFit = "fill";
+                    break;
+                case ImageFit.None:
+                    InnerElement.style.objectFit = "none ";
+                    break;
+                case ImageFit.ScaleDown:
+                    InnerElement.style.objectFit = "scale-down";
+                    break;
+            }
+
+            return this;
+        }
+    }
+
+    public enum ImageFit
+    {
+        Contain,
+        Cover,
+        Fill,
+        None,
+        ScaleDown
     }
 }
