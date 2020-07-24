2b9b8a681a61e18e3e54e6d7b4afa9341f2ba9a2
rafael-aero <rafael@rafael.aero>
2020-07-24 13:01:26 +0200

    should only check FullPath

--- Tesserae/src/Helpers/Routing/Router.cs
+++ Tesserae/src/Helpers/Routing/Router.cs
@@ -226,7 +226,7 @@ namespace Tesserae
         private static void LocationChanged(bool allowCallbackEvenIfLocationUnchanged)
         {
             var currentPathFromHash = (window.location.hash ?? "").TrimStart('#');
-            if ((_currentState is object) && (_currentState.Path?.TrimStart('#') == currentPathFromHash || _currentState.FullPath?.TrimStart('#') == currentPathFromHash) && !allowCallbackEvenIfLocationUnchanged)
+            if ((_currentState is object) && (_currentState.FullPath?.TrimStart('#') == currentPathFromHash) && !allowCallbackEvenIfLocationUnchanged)
                 return;
 
             var p = currentPathFromHash.Split(new[] { '?' }, count: 2); // Do not remove empty entries, as we need the empty entry in the array
