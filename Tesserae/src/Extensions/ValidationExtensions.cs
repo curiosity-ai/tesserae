a0ca7e21901e5aa48ea76a4b4681e57d2144a249
Dan Roberts <dangger36@gmail.com>
2020-10-05 15:23:32 +0100

    Replace a TODO comment with the intended explanation

--- Tesserae/src/Extensions/ValidationExtensions.cs
+++ Tesserae/src/Extensions/ValidationExtensions.cs
@@ -18,9 +18,13 @@ namespace Tesserae.Components
             // scream red validation error messages at them before they've even had a chance to start filling it in and that hardly seems polite
             // - So validation should only occur when a component's content is changed (which the component.Atttach call handles) or when the entire form is submitted, at which point there should be a validator instance whose "IsValid"
             //   property is checked and that will FORCE validation of all fields. If the User has left half of the mandatory fields blank at that point, THEN they can be shouted at about it.
-            // TODO: Explain WouldBeValid
+
+            // 2020-09-16 DWR: We now register TWO callbacks with the Validator - one to just check whether the component WOULD be considered valid and a second one to check that and then update the visual state accordingly. The second
+            // callback used to be the only one that we had and the first one has been added so that the Validator can implement an method "AreCurrentValuesAllValid" that peeks at the current state to see if the form has been pre-populated
+            // at all and would be considered valid but without displaying all the validation messages on inputs that the User hasn't touched yet for things that are NOT valid.
+
             component.Attach(_ => ApplyValidation());
-            validator?.Register(component, WouldBeValid, () => ApplyValidation());
+            validator?.Register(component, WouldBeValid, () => ApplyValidation());;
 
             return component;
 
