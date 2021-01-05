73c642125c7216cbbe5169b58d003d0a61f2b0ac
Pius Friesch <pius@curiosity.ai>
2021-01-05 19:15:54 +0100

    formatting

--- Tesserae/src/Components/ContextMenu.cs
+++ Tesserae/src/Components/ContextMenu.cs
@@ -7,12 +7,13 @@ namespace Tesserae
 {
     public sealed class ContextMenu : Layer<ContextMenu>, IContainer<ContextMenu, ContextMenu.Item>
     {
-        private readonly HTMLElement _childContainer;
-        private HTMLDivElement _modalOverlay;
-        private HTMLDivElement _popup;
+        private readonly HTMLElement    _childContainer;
+        private          HTMLDivElement _modalOverlay;
+        private          HTMLDivElement _popup;
+
         public ContextMenu()
         {
-            InnerElement = Div(_("tss-contextmenu"));
+            InnerElement    = Div(_("tss-contextmenu"));
             _childContainer = Div(_());
 
             InnerElement.onclick = (e) =>
@@ -59,21 +60,21 @@ namespace Tesserae
             {
                 _modalOverlay = Div(_("tss-contextmenu-overlay"));
                 _modalOverlay.addEventListener("click", _ => Hide());
-                _popup = Div(_("tss-contextmenu-popup"), _childContainer);
-                _contentHtml = Div(_(), _modalOverlay, _popup);
+                _popup       = Div(_("tss-contextmenu-popup"), _childContainer);
+                _contentHtml = Div(_(),                        _modalOverlay, _popup);
             }
 
             _popup.style.height = "unset";
-            _popup.style.left = "-1000px";
-            _popup.style.top = "-1000px";
+            _popup.style.left   = "-1000px";
+            _popup.style.top    = "-1000px";
 
             base.Show();
 
             if (!_popup.classList.contains("tss-no-focus")) _popup.classList.add("tss-no-focus");
 
-            var popupRect = (ClientRect)_popup.getBoundingClientRect();
-            _popup.style.left = x + "px";
-            _popup.style.top = y  + "px";
+            var popupRect = (ClientRect) _popup.getBoundingClientRect();
+            _popup.style.left     = x        + "px";
+            _popup.style.top      = y        + "px";
             _popup.style.minWidth = minWidth + "px";
 
             //TODO: CHECK THIS LOGIC
@@ -85,7 +86,7 @@ namespace Tesserae
                 {
                     if (y > window.innerHeight - y - 1)
                     {
-                        _popup.style.top = "1px";
+                        _popup.style.top    = "1px";
                         _popup.style.height = y - 1 + "px";
                     }
                     else
@@ -106,7 +107,7 @@ namespace Tesserae
                 {
                     if (x > window.innerWidth - x - 1)
                     {
-                        _popup.style.left = "1px";
+                        _popup.style.left  = "1px";
                         _popup.style.width = x - 1 + "px";
                     }
                     else
@@ -132,24 +133,24 @@ namespace Tesserae
             {
                 _modalOverlay = Div(_("tss-contextmenu-overlay"));
                 _modalOverlay.addEventListener("click", _ => Hide());
-                _popup = Div(_("tss-contextmenu-popup"), _childContainer);
-                _contentHtml = Div(_(), _modalOverlay, _popup);
+                _popup       = Div(_("tss-contextmenu-popup"), _childContainer);
+                _contentHtml = Div(_(),                        _modalOverlay, _popup);
             }
 
             _popup.style.height = "unset";
-            _popup.style.left = "-1000px";
-            _popup.style.top = "-1000px";
+            _popup.style.left   = "-1000px";
+            _popup.style.top    = "-1000px";
 
             base.Show();
 
             if (!_popup.classList.contains("tss-no-focus")) _popup.classList.add("tss-no-focus");
 
-            ClientRect parentRect = (ClientRect)element.getBoundingClientRect();
-            var popupRect = (ClientRect)_popup.getBoundingClientRect();
+            ClientRect parentRect = (ClientRect) element.getBoundingClientRect();
+            var        popupRect  = (ClientRect) _popup.getBoundingClientRect();
 
-            _popup.style.left     = parentRect.left + "px";
+            _popup.style.left     = parentRect.left               + "px";
             _popup.style.top      = parentRect.bottom - distanceY + "px";
-            _popup.style.minWidth = parentRect.width + "px";
+            _popup.style.minWidth = parentRect.width              + "px";
 
 
             //TODO: CHECK THIS LOGIC
@@ -161,7 +162,7 @@ namespace Tesserae
                 {
                     if (parentRect.top > window.innerHeight - parentRect.bottom - distanceY)
                     {
-                        _popup.style.top = "1px";
+                        _popup.style.top    = "1px";
                         _popup.style.height = parentRect.top - distanceY + "px";
                     }
                     else
@@ -182,7 +183,7 @@ namespace Tesserae
                 {
                     if (parentRect.left > window.innerWidth - parentRect.right - distanceX)
                     {
-                        _popup.style.left = "1px";
+                        _popup.style.left  = "1px";
                         _popup.style.width = parentRect.left - distanceX + "px";
                     }
                     else
@@ -257,22 +258,24 @@ namespace Tesserae
         public class Item : ComponentBase<Item, HTMLButtonElement>
         {
             private readonly HTMLElement _innerComponent;
+
             public Item(string text = string.Empty)
             {
                 _innerComponent = null;
-                InnerElement = Button(_("tss-contextmenu-item", text: text));
+                InnerElement    = Button(_("tss-contextmenu-item", text: text));
                 AttachClick();
                 InnerElement.addEventListener("mouseover", OnItemMouseOver);
             }
 
             public Item(IComponent component)
             {
-                if(component is ITextFormating itf && (itf is Button || itf is Link))
+                if (component is ITextFormating itf && (itf is Button || itf is Link))
                 {
                     itf.SetTextAlign(TextAlign.Left);
                 }
+
                 _innerComponent = component.WS().Render();
-                InnerElement = Button(_("tss-contextmenu-item"));
+                InnerElement    = Button(_("tss-contextmenu-item"));
                 InnerElement.appendChild(_innerComponent);
                 AttachClick();
                 InnerElement.addEventListener("mouseover", OnItemMouseOver);
@@ -292,7 +295,7 @@ namespace Tesserae
                     InnerElement.classList.add($"tss-contextmenu-{value.ToString().ToLower()}");
 
                     if (value == ItemType.Item) InnerElement.tabIndex = 0;
-                    else InnerElement.tabIndex = -1;
+                    else InnerElement.tabIndex                        = -1;
                 }
             }
 
@@ -348,18 +351,20 @@ namespace Tesserae
                 if (Type == ItemType.Item)
                 {
                     Clicked += e;
-                    if(_innerComponent is object)
+                    if (_innerComponent is object)
                     {
                         _innerComponent.onclick += (e2) =>
                         {
-                            if(_innerComponent.tagName != "A" || string.IsNullOrWhiteSpace(_innerComponent.As<HTMLAnchorElement>().href))
+                            if (_innerComponent.tagName != "A" || string.IsNullOrWhiteSpace(_innerComponent.As<HTMLAnchorElement>().href))
                             {
                                 StopEvent(e2); //Stop double calling the click handler for anything but links
                             }
+
                             e.Invoke(this, e2);
                         };
                     }
                 }
+
                 return this;
             }
 
