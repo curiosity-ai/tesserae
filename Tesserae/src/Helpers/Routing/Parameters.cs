7f864eaf7eb3608cfb6078319b9e7c940062035f
Pius Friesch <pius@curiosity.ai>
2021-03-01 18:40:24 +0100

    formatting

--- Tesserae/src/Helpers/Routing/Parameters.cs
+++ Tesserae/src/Helpers/Routing/Parameters.cs
@@ -8,12 +8,13 @@ namespace Tesserae
         public Parameters(Dictionary<string, string> parameters) => _parameters = parameters;
 
         public new string this[string key] => _parameters[key];
-        public IEnumerable<string> Keys => _parameters.Keys;
+
+        public IEnumerable<string> Keys   => _parameters.Keys;
         public IEnumerable<string> Values => _parameters.Values;
 
         public int Count => _parameters.Count;
 
-        public bool ContainsKey(string key) => _parameters.ContainsKey(key);
+        public bool ContainsKey(string key)                   => _parameters.ContainsKey(key);
         public bool TryGetValue(string key, out string value) => _parameters.TryGetValue(key, out value);
 
         public IEnumerator<KeyValuePair<string, string>> GetEnumerator() => _parameters.GetEnumerator();
