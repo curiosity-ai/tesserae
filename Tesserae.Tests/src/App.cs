6299ab1d500d3643faf853006e9afb54d9a25c76
theolivenbaum <rafael@rafael.aero>
2021-03-13 15:35:07 +0100

    Update App.cs

--- Tesserae.Tests/src/App.cs
+++ Tesserae.Tests/src/App.cs
@@ -123,7 +123,7 @@ namespace Tesserae.Tests
                 var closePanelButton = Button().SetIcon(LineAwesome.ArrowLeft).Tooltip("Close panel");
 
                 var component = components[componentRouteName]();
-                var splitView = SplitView().NoSplitter().PanelStyle()
+                var splitView = SplitView().NoSplitter()
                    .Left(Stack().Stretch().Children(MainNav(links, navBar, sideBar)).InvisibleScroll(), background: Theme.Default.Background)
                    .LeftIsSmaller(300.px())
                    .Stretch()
