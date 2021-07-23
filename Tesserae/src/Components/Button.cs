a6f8edfd4b0a21cc8cb91f593c6f2b39f56613e0
theolivenbaum <rafael@rafael.aero>
2021-07-23 08:23:39 +0200

    remove delay for focus for all places

--- Tesserae/src/Components/Button.cs
+++ Tesserae/src/Components/Button.cs
@@ -592,7 +592,7 @@ namespace Tesserae
         {
             // 2020-12-29 DWR: Seems like this setTimeout is required then the element is rendered within a container that uses "simplebar" scrolling - without the delay, if the element getting focus is out of view then it will not be
             // scrolled into view (even though it has successfully received focus)
-            DomObserver.WhenMounted(InnerElement, () => window.setTimeout((_) => InnerElement.focus(), 500));
+            DomObserver.WhenMounted(InnerElement, () => { InnerElement.scrollIntoViewIfNeeded(); InnerElement.focus(); });
             return this;
         }
 
