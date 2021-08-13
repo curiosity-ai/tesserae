318b6d5006c73dfaf394e8205c232e79b2b81c18
Pius Friesch <pius@curiosity.ai>
2021-08-13 14:48:00 +0200

    add fa icon with text size

--- Tesserae/src/Base/UI.Components.cs
+++ Tesserae/src/Base/UI.Components.cs
@@ -211,6 +211,8 @@ namespace Tesserae
 
         public static Icon Icon(string icon, string color = null) => new Icon(icon).Foreground(color ?? "");
 
+        public static Icon Icon(string icon, TextSize size = TextSize.Medium, string color = null) => new Icon($"{icon} tss-fontsize-{size.ToString().ToLower()}").Foreground(color ?? "");
+
         public static Icon Icon(LineAwesome icon, LineAwesomeWeight weight = LineAwesomeWeight.Light, TextSize size = TextSize.Medium, string color = null) => new Icon($"{weight} {icon} tss-fontsize-{size.ToString().ToLower()}").Foreground(color ?? "");
 
         public static HorizontalSeparator HorizontalSeparator(string text) => new HorizontalSeparator(text);
