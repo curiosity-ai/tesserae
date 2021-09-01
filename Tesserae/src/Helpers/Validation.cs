edc9c67adabb821e329845e68e5c12d08af3aacf
rafael-aero <rafael@rafael.aero>
2021-09-01 23:49:31 +0200

    fix sample validation rule

--- Tesserae/src/Helpers/Validation.cs
+++ Tesserae/src/Helpers/Validation.cs
@@ -15,9 +15,9 @@ namespace Tesserae
 
         public static string NonZeroPositiveInteger(TextBox textBox) => ((string.IsNullOrWhiteSpace(textBox.Text) || textBox.Text.Trim().Any(c => "0123456789".IndexOf(c) == -1) || !uint.TryParse(textBox.Text, out var numericValue) || numericValue == 0)) ? "must be a positive whole number, except zero".t() : null;
 
-        public static string LightColor(ColorPicker colorPicker) => colorPicker.Color.GetBrightness() >= 0.5f ? "must be a light color".t() : null;
+        public static string LightColor(ColorPicker colorPicker) => colorPicker.Color.GetBrightness() < 0.5f ? "must be a light color".t() : null;
 
-        public static string DarkColor(ColorPicker colorPicker) => colorPicker.Color.GetBrightness() <= 0.5f ? "must be a dark color".t() : null;
+        public static string DarkColor(ColorPicker colorPicker) => colorPicker.Color.GetBrightness() > 0.5f ? "must be a dark color".t() : null;
 
         public static string NotInThePast(DateTimePicker dateTimePicker) => dateTimePicker.DateTime < DateTime.Now ? "must not be in the past".t() : null;
 
