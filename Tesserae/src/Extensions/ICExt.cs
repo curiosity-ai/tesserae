0df7b1f72a51db30bc6fb93000e661501d761824
rafael-aero <rafael@rafael.aero>
2021-09-13 13:44:02 +0200

    Add tippy helper to show a tooltip immediatelly

--- Tesserae/src/Extensions/ICExt.cs
+++ Tesserae/src/Extensions/ICExt.cs
@@ -325,7 +325,7 @@ namespace Tesserae
             return component;
         }
 
-        public static T Tooltip<T>(this T component, string tooltip, TooltipAnimation animation = TooltipAnimation.ShiftAway, TooltipPlacement placement = TooltipPlacement.Top, int delayShow = 0, int delayHide = 0, bool followCursor = false) where T : IComponent
+        public static T Tooltip<T>(this T component, string tooltip, TooltipAnimation animation = TooltipAnimation.ShiftAway, TooltipPlacement placement = TooltipPlacement.Top, int delayShow = 0, int delayHide = 0, bool followCursor = false, int maxWidth = 350) where T : IComponent
         {
             if (string.IsNullOrWhiteSpace(tooltip))
                 return component;
@@ -336,7 +336,8 @@ namespace Tesserae
                 placement: placement,
                 delayShow: delayShow,
                 delayHide: delayHide,
-                followCursor: followCursor
+                followCursor: followCursor,
+                maxWidth : maxWidth
             );
         }
 
@@ -352,7 +353,7 @@ namespace Tesserae
             return component;
         }
         
-        public static T Tooltip<T>(this T component, IComponent tooltip, bool interactive = false, TooltipAnimation animation = TooltipAnimation.ShiftAway, TooltipPlacement placement = TooltipPlacement.Top, int delayShow = 0, int delayHide = 0, bool appendToBody = true, bool followCursor = false) where T : IComponent
+        public static T Tooltip<T>(this T component, IComponent tooltip, bool interactive = false, TooltipAnimation animation = TooltipAnimation.ShiftAway, TooltipPlacement placement = TooltipPlacement.Top, int delayShow = 0, int delayHide = 0, bool appendToBody = true, bool followCursor = false, int maxWidth = 350) where T : IComponent
         {
             if (tooltip is null)
                 return component;
@@ -383,11 +384,11 @@ namespace Tesserae
 
                 if (animation == TooltipAnimation.None)
                 {
-                    H5.Script.Write("tippy({0}, { content: {1}, interactive: {2}, placement: {3}, delay: [{4},{5}], appendTo: {6}, followCursor: {7} });", element, renderedTooltip, interactive, placement.ToString(), delayShow, delayHide, appendToBody ? document.body.As<object>() : "parent".As<object>(), followCursor);
+                    H5.Script.Write("tippy({0}, { content: {1}, interactive: {2}, placement: {3}, delay: [{4},{5}], appendTo: {6}, followCursor: {7}, maxWidth: {8} });", element, renderedTooltip, interactive, placement.ToString(), delayShow, delayHide, appendToBody ? document.body.As<object>() : "parent".As<object>(), followCursor, maxWidth);
                 }
                 else
                 {
-                    H5.Script.Write("tippy({0}, { content: {1}, interactive: {2}, placement: {3},  animation: {4}, delay: [{5},{6}], appendTo: {7}, followCursor: {8} });", element, renderedTooltip, interactive, placement.ToString(), animation.ToString(), delayShow, delayHide, appendToBody ? document.body.As<object>(): "parent".As<object>(), followCursor);
+                    H5.Script.Write("tippy({0}, { content: {1}, interactive: {2}, placement: {3},  animation: {4}, delay: [{5},{6}], appendTo: {7}, followCursor: {8}, maxWidth: {9} });", element, renderedTooltip, interactive, placement.ToString(), animation.ToString(), delayShow, delayHide, appendToBody ? document.body.As<object>(): "parent".As<object>(), followCursor, maxWidth);
                 }
 
                 H5.Script.Write("{0}._tippy.show();", element); //Shows it imediatelly, as the mouse is hovering the element
---
+++ Tesserae/src/Helpers/Tippy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Tesserae;
+using static H5.Core.dom;
+
+namespace Tesserae
+{
+    public static class Tippy
+    {
+        public static void ShowFor(IComponent component, IComponent tooltip, TooltipAnimation animation = TooltipAnimation.ShiftAway, TooltipPlacement placement = TooltipPlacement.Top, int maxWidth = 350)
+        {
+            var rendered = component.Render();
+
+            var renderedTooltip = UI.DIV(tooltip.Render());
+            renderedTooltip.style.display = "block";
+            renderedTooltip.style.overflow = "hidden";
+            renderedTooltip.style.textOverflow = "ellipsis";
+            document.body.appendChild(renderedTooltip);
+
+            var (element, _) = Stack.GetCorrectItemToApplyStyle(component);
+            if (element.HasOwnProperty("_tippy"))
+                H5.Script.Write("{0}._tippy.destroy();", element);
+
+            if (animation == TooltipAnimation.None)
+            {
+                H5.Script.Write("tippy({0}, { content: {1}, interactive: true, interactiveBorder: 8, placement: {2}, appendTo: {3}, maxWidth: {4}});", element, renderedTooltip, placement.ToString(), document.body.As<object>(), maxWidth);
+            }
+            else
+            {
+                H5.Script.Write("tippy({0}, { content: {1}, interactive: true, interactiveBorder: 8, placement: {2}, animation: {3},  appendTo: {4}, maxWidth: {5}});", element, renderedTooltip, placement.ToString(), animation.ToString(), document.body.As<object>(), maxWidth);
+            }
+
+            H5.Script.Write("{0}._tippy.show();", element);
+
+            // 2020-10-05 DWR: Sometimes a tooltip will be attached to an element that is removed from the DOM and then the tooltip is left hanging, orphaned. 
+            component.WhenRemoved(() =>
+            {
+                // 2020-10-05 DWR: I presume that have to check this property before trying to kill it in case it's already been tidied up
+                if (element.HasOwnProperty("_tippy"))
+                    H5.Script.Write("{0}._tippy.destroy();", element);
+            });
+        }
+    }
+}
