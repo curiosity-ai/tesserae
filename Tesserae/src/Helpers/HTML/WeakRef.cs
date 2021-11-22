5d2a18cb1952728b9e456c506ec3ef59cd9dd245
Pius Friesch <pius@curiosity.ai>
2021-11-22 17:05:37 +0100

    Add WeakRef

---
+++ Tesserae/src/Helpers/HTML/WeakRef.cs
@@ -0,0 +1,14 @@
+﻿using H5;
+
+namespace Tesserae
+{
+    [External]
+    public class WeakRef<T>
+    {
+        [Template("new WeakRef({data})")]
+        public extern WeakRef(T data);
+
+        [Template("{this}.deref()")]
+        public extern T Deref();
+    }
+}
