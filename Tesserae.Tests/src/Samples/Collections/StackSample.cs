6492decf4f0e5a43feef0fbd05ffacb8df386402
Pius Friesch <pius@curiosity.ai>
2024-03-22 14:49:36 +0100

    add a sortable stack component

--- Tesserae.Tests/src/Samples/Collections/StackSample.cs
+++ Tesserae.Tests/src/Samples/Collections/StackSample.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using H5.Core;
 using Tesserae;
 using static H5.Core.dom;
 using static Tesserae.UI;
@@ -12,16 +13,47 @@ namespace Tesserae.Tests.Samples
     {
         private readonly IComponent _content;
 
+        private const string sampleSortableStackLocalStorageKey = "sampleSortableStackOrder";
+
         public StackSample()
         {
-
             var mainButton = Button("Some Text").TextLeft().MinWidth(200.px()).Ellipsis().IconOnHover();
             mainButton.Tooltip("Tooltip for the main Button").SetIcon(UIcons.AngleLeft, Theme.Primary.Background);
 
-            var otherButton = Button().Tooltip("Tooltip for the other Button").SetIcon(UIcons.ThumbsDown, color: Theme.Danger.Background).Fade();
-            var hoverStack = HStack().MaxWidth(500.px()).Children(mainButton, otherButton);
+            var otherButton   = Button().Tooltip("Tooltip for the other Button").SetIcon(UIcons.ThumbsDown, color: Theme.Danger.Background).Fade();
+            var hoverStack    = HStack().MaxWidth(500.px()).Children(mainButton, otherButton);
+            var sortableStack = new SortableStack(Stack.Orientation.Horizontal).WS().AlignItemsCenter().PB(8).MaxWidth(500.px());
 
-            var stack = Stack();
+            sortableStack.Add("1", Button().SetIcon(UIcons._1));
+            sortableStack.Add("2", Button().SetIcon(UIcons._2));
+            sortableStack.Add("3", Button().SetIcon(UIcons._3));
+            sortableStack.Add("4", Button().SetIcon(UIcons._4));
+            sortableStack.Add("5", Button().SetIcon(UIcons._5));
+
+            var sortingTimeout = 0d;
+
+            sortableStack.OnSortingChanged(stackOrder =>
+                {
+                    window.clearTimeout(sortingTimeout);
+
+                    sortingTimeout = window.setTimeout(_ =>
+                    {
+                        localStorage.setItem(sampleSortableStackLocalStorageKey, es5.JSON.stringify(stackOrder));
+                        console.log("saved sorting for sortable stack sample", stackOrder);
+                    }, 1000);
+                }
+            );
+
+            var sortableStackSampleOrderJson = localStorage.getItem(sampleSortableStackLocalStorageKey);
+
+            if (sortableStackSampleOrderJson is object)
+            {
+                var sortableStackSampleOrder = es5.JSON.parse(sortableStackSampleOrderJson);
+                console.log("loaded sorting", sortableStackSampleOrder);
+                sortableStack.LoadSorting(sortableStackSampleOrder.As<string[]>());
+            }
+
+            var stack       = Stack();
             var countSlider = Slider(5, 0, 10, 1);
 
             _content = SectionStack()
@@ -51,6 +83,8 @@ namespace Tesserae.Tests.Samples
                                 )
                             )
                         ), stack.HeightAuto())))
+               .Section(Stack().Children(SampleTitle("Sortable"),
+                    Label("Stack with sortable elements").SetContent(sortableStack)))
                .Section(Stack().Children(SampleTitle("Advanced"),
                     Label("Stack with hover events").SetContent(hoverStack
                        .OnMouseOver((s, e) => otherButton.Show())
