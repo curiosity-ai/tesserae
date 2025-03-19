5e7994f68c1adb7dbda4fe5e5b4de12b5c38b310
theolivenbaum <rafael@curiosity.ai>
2025-03-19 08:29:06 +0100

    Update Program.cs

--- Build.InjectSamplesCode/Program.cs
+++ Build.InjectSamplesCode/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace Build.ImportInterfaceIcons
+namespace Build.InjectSamplesCode
 {
     static class Program
     {
