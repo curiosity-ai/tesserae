480f9eb2bd6485206581119f2c011a0973e67887
theolivenbaum <rafael@rafael.aero>
2026-09-03 11:00:34 +0000

    update_demo_70317

---
+++ Transpose.Core.js
@@ -0,0 +1,8 @@
+/**
+ * Transpose.Translator generated output.
+ */
+Transpose.assemblyVersion("Transpose.Core", "1.0.0.0");
+Transpose.assembly("Transpose.Core", function ($asm, globals) {
+    "use strict";
+
+});
